ef67a4c95bf1b052ec43d3afa63a103cd7dfacdc
akashvaghela09 <akashvaghela09@gmail.com>
2021-11-15 15:47:31 +0530

    routes updated

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from "../Styles/Home.module.css"
-import { loadData, saveData } from "../Utils/localStorage";
+import { loadData, saveData, clearData } from "../Utils/localStorage";
 
 const Home = () => {
     
     const history = useHistory();
+    const [username, setUsername] = useState("")
     
     if (loadData("email") === "guest"){
         history.push("/welcome");
     }
     
+    useEffect(() => {
+        let tempName = loadData("name")
+        setUsername(tempName)
+    }, []);
+    
+    const handleLogout = () => {
+        clearData()
+        saveData("email", "guest");
+        history.push("/welcome");
+    }
     return (
-        <div >
-            Homepage
+        <div className={styles.container}>
+            <h1>Hi {username} 👋</h1>
+            <button className={styles.button} onClick={() => handleLogout()}>LOGOUT</button>
         </div>
     )
 }
